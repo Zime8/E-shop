69a62a1942fcbee363c652158a6625e92756f819
Zime8 <148374327+Zime8@users.noreply.github.com>
2026-02-02 15:04:54 +0100

    Gestione warning

--- src/main/java/org/example/controllers/app/OrderSummaryAppController.java
+++ src/main/java/org/example/controllers/app/OrderSummaryAppController.java
@@ -29,7 +29,7 @@ public class OrderSummaryAppController {
                             it.getProductName(), it.getSize(), it.getQuantity(), unit, it.getProductImage(), subtotal
                     ));
                 } catch (Exception e) {
-                    logger.log(Level.WARNING, "Errore process CartItem: " + it, e);
+                    logger.log(Level.WARNING, "Errore process CartItem: {0} ", it);
                 }
             }
         }
